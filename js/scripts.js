53c8b3f67e3554a395e97b3210d86dedd1f13030
jrrrsmith <jeremy.scott.smith@me.com>
2019-04-03 19:14:21 -0400

    initial commit of files

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	$thumbnail = $('.gallery__image');
+	$modal = $('.modal');
+	$modal_image = $('.modal__image');
+	$modal_close = $('.modal-close');
+	$next_button = $('.next-button');
+	$prev_button = $('.prev-button');
+
+	$thumbnail.click(function() {
+
+		$current_img = $(this);
+		img_src = $(this).attr('src');
+
+		$modal_image.attr('src', img_src);
+		$modal.addClass('modal--open');
+
+	});
+
+	$modal_close.click(function() {
+		$modal.removeClass('modal--open');
+		$modal_image.attr('src', '');
+	});
+
+});
